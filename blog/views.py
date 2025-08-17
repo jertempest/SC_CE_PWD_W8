20d7159863bcdee166911b14190706227ada0471
Jeremy <tempestj@sheridancollege.ca>
2025-08-16 20:19:55 -0400

    final commit for week 8

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from . import models
 
 # Create your views here.
+def home(request):
+    """
+    The Blog Hompage
+    """
+    # Get the last 3 posts
+    latest_posts = models.Post.objects.published().order_by('-published')[:3]
+    # Add context variable "latest_posts"
+    context = {'latest_posts': latest_posts}
+    return render(request, 'blog/home.html', context)
